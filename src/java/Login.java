ee57f0f63064dde404c1a50bee8cbaccbeb964aa
Natalie Wagner <nat82alie@yahoo.com>
2018-05-08 15:36:56 -0700

    selector works based on whether customer/empl/admin

--- src/java/Login.java
+++ src/java/Login.java
@@ -9,6 +9,11 @@ import javax.faces.component.UIInput;
 import javax.faces.context.FacesContext;
 import javax.faces.validator.ValidatorException;
 import javax.inject.Named;
+import javax.swing.JFrame;
+
+import java.io.Serializable;
+import java.util.*;
+import javax.xml.bind.annotation.*;
 
 /**
  *
@@ -22,7 +27,16 @@ public class Login extends DBConnect implements Serializable {
     private String login;
     private String password;
     private UIInput loginUI;
-
+    public String userType;
+    
+    public String getUserType() {
+        return userType;
+    }
+    
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+    
     public UIInput getLoginUI() {
         return loginUI;
     }
@@ -51,6 +65,7 @@ public class Login extends DBConnect implements Serializable {
             throws ValidatorException, SQLException {
         DBConnect dbc = new DBConnect();
         Connection con = dbc.getConnection();
+        con.setAutoCommit(false);
         
         login = loginUI.getLocalValue().toString();
         password = value.toString();
@@ -61,7 +76,7 @@ public class Login extends DBConnect implements Serializable {
             ResultSet res = stmt.executeQuery(selectStmt);
             
             if (!res.next()) {
-                selectStmt = "select * from Customer where Login='" + login + "' and Pwd='" + password + "'";
+                selectStmt = "select * from Customers where Login='" + login + "' and Pwd='" + password + "'";
                 try (Statement s = con.createStatement()) {
                     s.execute(selectStmt);
                     res = stmt.executeQuery(selectStmt);
@@ -71,12 +86,18 @@ public class Login extends DBConnect implements Serializable {
                         throw new ValidatorException(errorMessage);
                     }
                     
+                    setUserType("customer");
                     go();
                 } catch (SQLException e) {
                     con.rollback();
                 }
             }
             
+            boolean isAdmin = res.getBoolean("Admin");
+            if (isAdmin)
+                setUserType("admin");
+            else if (!isAdmin)
+                setUserType("employee");
             go();
             
         } catch (SQLException e) {
